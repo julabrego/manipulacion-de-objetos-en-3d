c1444e2611956ab737debef98351d8ee1dcbf8a5
julabrego <abrego.jul@gmail.com>
2025-08-08 20:22:08 -0300

    feat: supports both hands grabbing

--- TDPV_MOD3D_ACTIVIDAD_FINAL_ABREGO/Assets/Scripts/Gameplay/GrabItem.cs
+++ TDPV_MOD3D_ACTIVIDAD_FINAL_ABREGO/Assets/Scripts/Gameplay/GrabItem.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class GrabItem : MonoBehaviour
 {
-    public GameObject grabbingItem;
     public Camera playerCamera;
-
-    private bool isInGrabArea = false;
+    
+    public GameObject grabbingItemWithRightHand;
+    public GameObject grabbingItemWithLeftHand;
     private GameObject itemToGrab;
+
     public FPSController playerController;
+    private bool isInGrabArea = false;
 
     int LEFT_BUTTON = 0;
     int RIGHT_BUTTON = 1;
@@ -22,13 +24,17 @@ public class GrabItem : MonoBehaviour
             return;
         }
 
-        if (isInGrabArea && Input.GetMouseButton(RIGHT_BUTTON) && grabbingItem == null)
+        if (isInGrabArea && Input.GetMouseButton(RIGHT_BUTTON) && grabbingItemWithRightHand == null)
         {
-            grabbingItem = itemToGrab;
+            grabbingItemWithRightHand = itemToGrab;
         }
-        else if (grabbingItem != null && !Input.GetMouseButton(RIGHT_BUTTON))
+        else if (isInGrabArea && Input.GetMouseButton(LEFT_BUTTON) && grabbingItemWithLeftHand == null)
         {
-            Rigidbody rb = grabbingItem.GetComponent<Rigidbody>();
+            grabbingItemWithLeftHand = itemToGrab;
+        }
+        else if (grabbingItemWithRightHand != null && !Input.GetMouseButton(RIGHT_BUTTON))
+        {
+            Rigidbody rb = grabbingItemWithRightHand.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 rb.useGravity = true;
@@ -42,18 +48,48 @@ public class GrabItem : MonoBehaviour
                 rb.velocity = throwDirection * launchSpeed + playerVelocity * 0.6f; // opcional: suma parte de la velocidad
             }
 
-            grabbingItem = null;
+            grabbingItemWithRightHand = null;
+        }
+        else if (grabbingItemWithLeftHand != null && !Input.GetMouseButton(LEFT_BUTTON))
+        {
+            Rigidbody rb = grabbingItemWithLeftHand.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.useGravity = true;
+
+                // TODO: Extract this blockto a self descriptive function
+                Vector3 throwDirection = playerCamera.transform.forward.normalized;
+                // Obtener la velocidad real del jugador
+                Vector3 playerVelocity = playerController.currentVelocity;
+                // Usar la magnitud de la velocidad como fuerza hacia donde mira
+                float launchSpeed = playerVelocity.magnitude;
+                rb.velocity = throwDirection * launchSpeed + playerVelocity * 0.6f; // opcional: suma parte de la velocidad
+            }
+
+            grabbingItemWithLeftHand = null;
         }
 
-        if (grabbingItem != null)
+        if (grabbingItemWithRightHand != null)
         {
             // TODO: Extract this blockto a self descriptive function
             Vector3 holdOffset = playerCamera.transform.forward * 0.5f
                 + playerCamera.transform.up * -0.3f
                 + playerCamera.transform.right * 0.3f;
-            grabbingItem.transform.SetPositionAndRotation(
+            grabbingItemWithRightHand.transform.SetPositionAndRotation(
                 playerCamera.transform.position + holdOffset,
                 Quaternion.LookRotation(playerCamera.transform.forward) * Quaternion.Euler(-45f, -75f, 0f)
+            );
+        }
+
+        if (grabbingItemWithLeftHand != null)
+        {
+            // TODO: Extract this blockto a self descriptive function
+            Vector3 holdOffset = playerCamera.transform.forward * 0.5f
+                + playerCamera.transform.up * -0.3f
+                + playerCamera.transform.right * -0.3f;
+            grabbingItemWithLeftHand.transform.SetPositionAndRotation(
+                playerCamera.transform.position + holdOffset,
+                Quaternion.LookRotation(playerCamera.transform.forward) * Quaternion.Euler(-45f, 75f, 0f)
             );
         }
     }
