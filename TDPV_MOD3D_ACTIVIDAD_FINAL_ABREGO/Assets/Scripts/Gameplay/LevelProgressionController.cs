b399a9d868642895399b79751892233716ee57bf
julabrego <abrego.jul@gmail.com>
2025-09-29 20:59:28 -0300

    fix: adjustments in ui sizing

--- TDPV_MOD3D_ACTIVIDAD_FINAL_ABREGO/Assets/Scripts/Gameplay/LevelProgressionController.cs
+++ TDPV_MOD3D_ACTIVIDAD_FINAL_ABREGO/Assets/Scripts/Gameplay/LevelProgressionController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Profiling;
-using static UnityEditor.Timeline.Actions.MenuPriority;
 
 public class LevelProgressionController : MonoBehaviour
 {
